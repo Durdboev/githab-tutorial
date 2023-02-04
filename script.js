933a27032441dc819c685ce3013ac44b71b64385
Durboev <jamshiddurboyev@gmail.com>
2023-02-05 01:38:45 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// const channel = prompt("What is your favourite YouTuber?")
+
+// console.log(`https://youtube.com/${channel}`);
+
+//const user = "Jamshid";
+//const job = "Student";
+
+//console.log(`user name is ${user}, he is ${job}`);
+
+//console.log("Jamshid" + "Durboyev")
+//console.log("9" + "3")
+
+//let incr = 10;
+// let decr = 10;
+// console.log(incr++);
+// console.log(decr--);
+// console.log(incr++);
+// console.log(decr--);
+// console.log(incr);
+// console.log(decr);
+// console.log(49 % 9);
+//console.log("5*5"===25);
+
+// const isAge = true
+// const isClose = true
+// console.log(isAge && !isClose)
+// console.log(isAge || !isClose)
+//console.log(2+2*2=="8");
